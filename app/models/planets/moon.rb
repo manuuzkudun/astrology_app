fba6a2bee25b3f7cb4d9f0fe028117eb7606143d
Manu Uzkudun <manuuzkudun@hotmail.com>
2019-04-11 22:46:36 +0200

    pilgrim planet measure added

--- app/models/planets/moon.rb
+++ app/models/planets/moon.rb
@@ -3,6 +3,7 @@ module Planets
     DOMICILES = %w(cancer).freeze
     EXALTATION = 'taurus'.freeze
     DETRIMENTS = %w(capricorn).freeze
+    PILGRIM_SIGNS = %w(aries gemini leo virgo libra sagittarius aquarius).freeze
     FALL = 'scorpio'.freeze
     JOY = 1
     AVRG_SPEED = 0.5
@@ -11,10 +12,11 @@ module Planets
 
     def initialize(params)
       super(params)
-      @domiciles = get_domiciles(DOMICILES)
-      @exaltation = get_exaltation(EXALTATION)
-      @detriments = get_detriments(DETRIMENTS)
-      @fall = get_fall(FALL)
+      @domiciles = get_signs(DOMICILES)
+      @exaltation = get_sign(EXALTATION)
+      @detriments = get_signs(DETRIMENTS)
+      @fall = get_sign(FALL)
+      @pilgrim_signs = get_signs(PILGRIM_SIGNS)
     end
 
     def in_joy?
