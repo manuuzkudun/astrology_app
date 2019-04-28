f0c3a7c678610e19dc61bdd847521e3977ce40c2
Manu Uzkudun <manuuzkudun@hotmail.com>
2019-04-28 14:15:46 +0200

    dignity calculator refractored

--- app/models/dignity_calculator.rb
+++ app/models/dignity_calculator.rb
@@ -16,26 +16,36 @@ class DignityCalculator
     results = {}
     total_score = 0
     DIGNITY_SCORES.each do |dignity|
-      method = @dignity_methods[dignity]['method']
-      score = @dignity_scores[dignity]['score']
-
-      if dignity == 'dispositor'
-        planet_count = planet.send(method, @planets)
-        dignified = planet_count > 0
-        score = score * planet_count
-      elsif dignity == 'domicile_reception' || dignity == 'exaltation_reception'
-        dignified = planet.send(method, @planets)
-      else
-        dignified = planet.send(method)
-      end
-
-      total_score += score if dignified
+      score = get_dignity_score(planet, dignity)
+      total_score += score
       results[dignity.to_sym] = {
-        result: dignified,
-        score: dignified ? score : 0
+        result: !score.zero?,
+        score: score
       }
     end
     results["total_score"] = total_score
     results
+  end
+
+  private
+
+  def get_dignity_score(planet, dignity)
+    method = @dignity_methods[dignity]['method']
+    score = @dignity_scores[dignity]['score']
+
+    case dignity
+    when 'dispositor'
+      planet_count = planet.send(method, @planets)
+      dignified = planet_count > 0
+      score =  score * planet_count
+    when 'domicile_reception'
+      dignified = planet.send(method, @planets)
+    when 'exaltation_reception'
+      dignified = planet.send(method, @planets)
+    else
+      dignified = planet.send(method)
+    end
+
+    return dignified ? score : 0
   end
 end
