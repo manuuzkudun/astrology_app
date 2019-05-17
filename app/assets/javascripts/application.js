417ded1d60a7e55860a06806bb1995fc7daea924
astro_man <polemos@tuta.io>
2019-05-17 19:52:05 +0200

    Ended date picker plus map implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,9 +20,42 @@
 DECLARE GLOBAL VARS
 ================================================== */
 
-let myFormGroup, //Var to hold a reference for the form group so we can populate its fileds according to changes on datepicker
+let mapQuestKey = 'fGs90Xaz6Ar09AEHLz7wgjjwTWV7vFJz', //MapQuest API Key
+    myMapQuestForm, //Var to hold a reference for the form taking care of geolocation
+    myMapQuestButton, //Var to hold a reference for the Map Quest Button to submit data to API
+    myMapQuestCloser, //Var to hold a reference for the button closing the map
+    myMap, //Var to hold a refernce for the div holding the map
+    myMapMarker, //Var to hold a reference for the MapQuest Markers
+    mySelects, //Var to hold a reference for the different select form fields
+    myFormGroup, //Var to hold a reference for the form group so we can populate its fileds according to changes on other DOM NODES
     myInputs, //Var to hold a reference for all the inputs containing user data
     mySubmitBtn; //Var to hold a reference for the Submit Button
+
+const myMarkerIconOptions = {
+  primaryColor: '#22407F',
+  secondaryColor: '#3B5998',
+  shadow: false,
+  size: 'md'
+}
+
+/* ==================================================
+LOCAL Fetching FUNCTION
+================================================== */
+
+class Fetch {
+  async apiCall(_url, _data) {
+
+
+    const call = await fetch(_url),
+          data = await call.json();
+
+    return data;
+  }
+}
+
+const fetchC = new Fetch(); //Custom Fecth Class
+
+
 
 /* ==================================================
 UTILS FUNCTION
@@ -31,34 +64,207 @@ UTILS FUNCTION
 const leadingStringToZero = (_str) => {
   _str.length < 2 ? _str = '0' + _str : null;
   return _str;
+},
+insertAfter = (_newNode, _referenceNode) => {
+    _referenceNode.parentNode.insertBefore(_newNode, _referenceNode.nextSibling);
+}
+
+
+/* ==================================================
+MAPQUEST FUNCTION
+================================================== */
+
+const mapQuestCall = (_countryCode, _cityName) => {
+
+  console.log('city name lengh is ' + _cityName.length)
+
+  !_cityName.length  ? _cityName = 'a' : null;
+
+  L.mapquest.key = mapQuestKey;
+
+  myMap ? myMap.remove() : null;
+
+  console.log(_countryCode, _cityName);
+
+  //!document.getElementById('map_mq').classList.contains('show') ? document.getElementById('map_mq').classList.add('show') : null;
+
+  //https://www.mapquestapi.com/geocoding/v1/address?key=KEY&inFormat=kvp&outFormat=json&location=La+roca+del+valles%2C+es&thumbMaps=false
+
+  /* BUILD UP THE QUERY
+  =================================================== */
+
+  // let myQuery = 'https://www.mapquestapi.com/geocoding/v1/address?key='+ mapQuestKey +'&inFormat=kvp&outFormat=json&location='+ _cityName +','+ _countryCode +'thumbMaps=false&maxResults=5';
+  //
+  // fetchC.apiCall(myQuery)
+  // .then(res => {
+  //
+  //   myMap ? myMap.remove() : null;
+  //
+  //   //console.log(res.results[0].locations[0].latLng);
+  //
+  //   let lat = res.results[0].locations[0].latLng.lat,
+  //       lng = res.results[0].locations[0].latLng.lng;
+  //
+  //   myMap = L.mapquest.map('map_mq', {
+  //     center: [lat, lng],
+  //     layers: L.mapquest.tileLayer('dark'),
+  //     zoom: 12
+  //   });
+  //
+  //   //myMap.addControl(L.mapquest.control());
+  //
+  //   L.marker([lat,lng], {
+  //     icon: L.mapquest.icons.marker({
+  //       primaryColor: '#22407F',
+  //       secondaryColor: '#3B5998',
+  //       shadow: false,
+  //       size: 'md'
+  //     })
+  //   }).addTo(myMap);
+  //
+  // })
+  // .catch(err => console.log('something went wrong ', err));
+
+  //L.mapquest.geocoding().geocode(_cityName +', '+ _countryCode, (error, response) => {
+  L.mapquest.geocoding().geocode({city:_cityName, country:_countryCode}, (error, response) => {
+
+    console.log(response);
+
+    let location = response.results[0].locations[0],
+        lat = location.latLng.lat,
+        lng = location.latLng.lng;
+        latLng = location.displayLatLng;
+
+
+    myMap = L.mapquest.map('map_mq', {
+      center: latLng,
+      layers: L.mapquest.tileLayer('dark'),
+      zoomControl : false,
+      zoom: 12
+    }).addEventListener('click', (e) => {
+      console.info(e, 'clicked on map');
+
+      myMap.removeLayer(myMapMarker);
+      myMapMarker = new L.marker(e.latlng, {
+        icon: L.mapquest.icons.marker(myMarkerIconOptions)
+      }).addTo(myMap);
+
+      setFormCoordinates(e.latlng.lat,e.latlng.lng);
+
+    });
+
+    myMapMarker = L.marker([lat,lng], {
+       icon: L.mapquest.icons.marker(myMarkerIconOptions)
+     }).addTo(myMap);
+
+     //L.mapquest.geocodingControl().addTo(myMap);
+
+     setFormCoordinates(lat,lng);
+
+
+
+
+
+
+
+  });
+
+
+
+
 }
 
 /* ==================================================
 LOGIC FUNCTION
 ================================================== */
 
-const onInputLeave = e => {
+const setFormCoordinates = (_lat,_lng) => {
+  document.getElementById('latitude').value = _lat;
+  document.getElementById('longitude').value = _lng;
 
-  //Basic Validation
+  // Experimental visual info for user once lat, long are set
+  // L.mapquest.key = mapQuestKey;
+  //
+  // L.mapquest.geocoding().reverse({lat:_lat,lng:_lng}, (error, response) => {
+  //   console.log('reverse response is ');
+  //   console.dir(response);
+  // });
 
-  let totalFilledInputs = 0;
 
-  myInputs.forEach(function(el,i){
-    el.value.length > 0 ? totalFilledInputs++ : null;
-  });
 
-  //Re-enable submit button if all inputs are filled
 
-  if (totalFilledInputs == myInputs.length ) {
+  document.querySelector('input.msg-mq').value = 'Located at ' + _lat.toFixed(2) + ' lat, ' + _lng.toFixed(2) + ' lng';
+
+  !document.querySelector('div.mq-ui--miniform').classList.contains('show-msg') ? document.querySelector('div.mq-ui--miniform').classList.add('show-msg') : null;
+
+  isFormReady();
+
+},
+isFormReady = () => {
+  let ready = true; //Var to keep track of the form readinees
+
+  mySelects.map( (_obj) => {
+    _obj.value == 'none' ? ready = false : null; //If there is any unset select set the form to not ready
+  } );
+
+  //if we are not showing the map locked message we do not have geo coordinates. Form is not ready
+
+  //console.log(document.querySelector('div.mq-ui--miniform').classList);
+
+  !document.querySelector('div.mq-ui--miniform').classList.contains('show-msg') ? ready = false : null;
+
+  //Enable/Disable Submit button based on form readiness
+
+  if(ready) {
     mySubmitBtn.classList.contains('disabled') ? mySubmitBtn.classList.remove('disabled') : null;
   }else {
     !mySubmitBtn.classList.contains('disabled') ? mySubmitBtn.classList.add('disabled') : null;
   }
 
+
+},
+toggleCityInput = () => {
+
+  if(document.querySelector('select.country-mq').value != 'none'){
+
+    document.querySelector('div.mq-ui').classList.contains('disabled') ? document.querySelector('div.mq-ui').classList.remove('disabled') : null;
+  }else {
+    !document.querySelector('div.mq-ui').classList.contains('disabled') ? document.querySelector('div.mq-ui').classList.add('disabled') : null;
+  }
+
+  //Reset the city input field on every country select change
+  document.querySelector('input.city-mq').value = '';
+  //Reset the city to accept new values
+  document.querySelector('div.mq-ui--miniform').classList.contains('show-msg') ? document.querySelector('div.mq-ui--miniform').classList.remove('show-msg') : null;
+
+
+},
+onInputLeave = e => {
+
+  //Basic Validation
+  //
+  // let totalFilledInputs = 0;
+  //
+  // myInputs.forEach(function(el,i){
+  //   el.value.length > 0 ? totalFilledInputs++ : null;
+  // });
+  //
+  // //Re-enable submit button if all inputs are filled
+  //
+  // if (totalFilledInputs == myInputs.length ) {
+  //   mySubmitBtn.classList.contains('disabled') ? mySubmitBtn.classList.remove('disabled') : null;
+  // }else {
+  //   !mySubmitBtn.classList.contains('disabled') ? mySubmitBtn.classList.add('disabled') : null;
+  // }
+
+  //Here we perform MaqQuest Search for location
+
+  console.log(e.target.value);
+
+  mapQuestCall(myMapQuestForm.querySelector('select').value, myMapQuestForm.querySelector('input.city-mq').value);
+
 },
 addInteractions = () => {
-
-  // console.log(mySubmitBtn);
 
   //Remove any alert if present sequentially by 2 second interval/delay
 
@@ -79,7 +285,121 @@ addInteractions = () => {
 
   //Only attempt to add interactions if we are on the new page (pathname agnostic)
 
-  if(mySubmitBtn && myInputs.length > 0) {
+  if(mySubmitBtn && mySelects.length > 0) {
+
+    mySelects.map((_obj) => {
+      _obj.addEventListener('change', (e) => {
+        console.log(e, _obj.value);
+
+        let mySelectedValue = _obj.value, //Store the value that just changed
+            mySubmitFormTarget = _obj.classList[1]; //Get the last className from the select classList which will refer to the target in which we need to place the right values for submiting the form
+
+        switch (mySubmitFormTarget) {
+          case 'country-mq':
+            console.log('changed the country select');
+
+
+
+            //Activate the input text field on Country select changed
+            toggleCityInput();
+          break;
+          case 'day':
+            console.log('changed the day select');
+            document.getElementById('day').value = _obj.value; //Set day to submit form
+          break;
+          case 'month':
+            console.log('changed the month select');
+            document.getElementById('month').value = _obj.value; //Set month to submit form
+          break;
+          case 'year':
+            console.log('changed the year select');
+            document.getElementById('year').value = _obj.value; //Set year to submit form
+          break;
+          case 'hour':
+            console.log('changed the hour select');
+            document.getElementById('hour').value = _obj.value + ' : ' + document.querySelector('select.form-control.minutes').value; //Set hour + minutes to submit form
+          break;
+          case 'minutes':
+            console.log('changed the minutes select');
+            document.getElementById('hour').value = document.querySelector('select.form-control.hour').value + ' : ' +  _obj.value ;//Set hour + minutes to submit form
+          break;
+          default:
+            null;
+          break;
+        }
+
+        isFormReady();
+
+      });
+
+
+    });
+
+
+
+
+    myMapQuestButton.addEventListener('click', (e) => {
+
+
+    // if( myMapQuestForm.querySelector('select').value != "0" && myMapQuestForm.querySelector('input').value.length > 0 ) {
+    //
+    //   console.log('Country code is ' + myMapQuestForm.querySelector('select').value + ' and city name is ' + myMapQuestForm.querySelector('input').value );
+    //
+    //   mapQuestCall(myMapQuestForm.querySelector('select').value, myMapQuestForm.querySelector('input').value);
+    //
+    // }else {
+    //   console.log('more data needed');
+    // }
+
+    //console.info(document.querySelector('select.country-mq').value);
+
+    if(document.querySelector('select.country-mq').value == 'none'){
+
+      let myAlert = document.createElement('p');
+
+      myAlert.className = 'alert alert-danger';
+      myAlert.innerHTML = 'Please, first select a Country to locate on Map';
+
+      insertAfter(myAlert, document.querySelector('ul.nav')); //insert Alert on top of page
+
+      setTimeout( () => {
+        myAlert.classList.add('faded');
+      }, 2000);
+
+
+    }else {
+
+      //Open Map
+
+      !document.body.classList.contains('has-map') ? document.body.classList.add('has-map') : null;
+
+
+
+      console.log(myMapQuestForm.querySelector('input.city-mq').value.length);
+
+      //Only make a blind call to API (no city) if there is no data in its input field and map is not already locked somewhere
+
+      if(myMapQuestForm.querySelector('input.city-mq').value.length < 1 && !document.querySelector('div.mq-ui--miniform').classList.contains('show-msg')) {
+
+        setTimeout(function () {
+          mapQuestCall(myMapQuestForm.querySelector('select').value, myMapQuestForm.querySelector('input.city-mq').value);
+        }, 1000); //Send the call with a one second delay so mapcan me reset properly when calling from on blur event as well
+
+
+      }
+
+
+
+    }
+
+    });
+
+
+    myMapQuestCloser.addEventListener('click', (e) => {
+      document.body.classList.contains('has-map') ? document.body.classList.remove('has-map') : null;
+    });
+
+
     //Normalize SubmitButton
 
     // Catching =  false is not working on root?
@@ -92,70 +412,7 @@ addInteractions = () => {
       _obj.addEventListener('blur', onInputLeave, false);
     });
 
-
-    let initDate = new Date(1949, 12, 1);
-    let config = {
-      target: 'mtr-datepicker',
-      smartHours: false,
-      disableAmPm: true,
-      animations: true,
-      timestamp: initDate.getTime(),
-      minutes: {
-        min: 0,
-        max: 55,
-        step: 10
-      },
-      months: {
-        min: 0,
-        max: 11,
-        step: 1
-      },
-      years: {
-        min: 1950,
-        max: 2050,
-        step: 1
-      }
-    };
-    let myDatepicker = new MtrDatepicker(config);
-
-    myDatepicker.onChange('all', function(e) {
-
-      switch (e) {
-        case 'day':
-          console.log('day switched to ', document.querySelector('input.mtr-input.dates').value);
-          document.getElementById('day').value = document.querySelector('input.mtr-input.dates').value;
-        break;
-        case 'month':
-          console.log('months switched to ', document.querySelector('input.mtr-input.months').value);
-          // For some reason months are 0 based indexed...just add 1 to the result
-          document.getElementById('month').value = parseInt(document.querySelector('input.mtr-input.months').value) + 1;
-        break;
-        case 'year':
-          console.log('years switched to ', document.querySelector('input.mtr-input.years').value);
-          document.getElementById('year').value = document.querySelector('input.mtr-input.years').value;
-        break;
-        case 'hour':
-          console.log('hours switched to ', document.querySelector('input.mtr-input.hours').value);
-          document.getElementById('hour').value = document.querySelector('input.mtr-input.hours').value + ':' + leadingStringToZero(document.querySelector('input.mtr-input.minutes').value) ;
-        break;
-        case 'minute':
-          console.log('minutes switched to ', document.querySelector('input.mtr-input.minutes').value);
-          document.getElementById('hour').value = document.querySelector('input.mtr-input.hours').value + ':' + leadingStringToZero(document.querySelector('input.mtr-input.minutes').value);
-        break;
-        default:
-        //console.log(typeof e, e)
-        null;
-        break;
-
-      }
-
-    });
-
-
-
   }
-
-
 
 },
 init = e => {
@@ -164,8 +421,12 @@ init = e => {
 
   document.removeEventListener('turbolinks:load', init, false);
   //Store/Assign DOM Elements
+  myMapQuestForm = document.querySelector('div.formgroup.secondary');
+  myMapQuestButton = document.getElementById('btn-mq');
+  myMapQuestCloser = document.getElementById('closer-mq');
   mySubmitBtn = document.getElementById('btn-submit');
-  myInputs = Array.from(document.querySelectorAll('div.formgroup.primary input[type="text"]'));
+  mySelects = Array.from(document.querySelectorAll('div.formgroup.secondary select'));
+  myInputs = Array.from(document.querySelectorAll('div.formgroup.secondary input[type="text"]'));
 
   addInteractions();
 
